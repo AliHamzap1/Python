0d5d21e76e69b26d31b89b92c0bd04e483ef7389
Ali Hamza <alihamza.aimviz99@gmail.com>
2025-05-31 18:53:05 +0500

    String Updated

--- 02_Basics-DataTypes/09_StrConversion.py
+++ 02_Basics-DataTypes/09_StrConversion.py
@@ -1,3 +1,6 @@
+from datetime import datetime       # Import dattime for current_year
+
+# Type Conversion
 print("How to convert string to int, and int to string")
 
 print("Convert str to int")
@@ -7,3 +10,12 @@ b = int(a)
 
 c = type(b)
 print(c)
+
+
+
+# Exercise
+birth_year = input("Which year you born?\n")        #return string
+current_year = datetime.now().year                  # Get current year as an integer
+age = current_year - int(birth_year)                # Convert string to int
+
+print(f"Your age is {age} years.")
