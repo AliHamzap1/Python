feb0e1b2dfa05fd982f721e1b2ecfc1d519193a4
Ali Hamza <alihamza.aimviz99@gmail.com>
2025-06-02 18:36:03 +0500

    2 June 2025

--- 02_Basics-DataTypes/10_formatted_str.py
+++ 02_Basics-DataTypes/10_formatted_str.py
@@ -1,3 +1,4 @@
+# Formatted String
 print("Formatted String")
 
 name = "Ali"
@@ -7,7 +8,7 @@ print('Python 2 way:')
 print('Hi, my name is ' + name + ' and I\'m '+ str(age) + ' Years old.')
 
 print('Python 3 way 1:')
-print(f'Hi, my name is {name} and I\'m {age} Years old.')
+print(f'Hi, my name is {name} and I\'m {age} Years old.')       # mostly recommended way in Python 3.6+
 
 print('Python 3 way 2:')
 print('Hi, my name is {} and I\'m {} Years old.' .format(name, age) )
